7109f4229093d451f2917262a79d760a6e82e7e9
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2021-02-01 23:29:58 +0530

    ADDED DATABASE

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
-let express  = require("express");
-let mongoose = require("mongoose");
-let bodyParser = require("body-parser");
-let app = express();
+var express=require("express");
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+var app=express();
+
 app.set('view engine','ejs');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true}))
-// var i1=[];
-mongoose.connect("mongodb://localhost:27071/todolistDB,{useNewUrlParser:true,useUnifiedTopology:true"});
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
 const itemSchema={
     name:String
 }
-const Item = mongoose.model("Item",itemSchema);
-
+const Item=mongoose.model("Item",itemSchema);
 const item1=new Item({
-    name:"Hello, Welcome to my word",
+    name:"StemPedia",
+});
+
+const d=[item1];
+
+app.get("/",function(req,res)
+{
+
+   Item.find({},function(err,f)
+   {
+
+      if(f.length===0)
+      {
+        Item.insertMany(d,function(err)
+        {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Successfully saved items to DB");
+            }
+        });
+      res.redirect("/");
+      }
+      else{
+      res.render("list",{newListItems:f});
+      }
+   })
+  ;
 })
+app.post("/",function(req,res)
+{
+     const itemName=req.body.n;
+     const item=new Item({
+     name:itemName
+   });
+item.save();
+res.redirect("/");
+});
+app.post("/delete",function(req,res)
+{
+  const check=req.body.checkbox;
+  Item.findByIdAndRemove(check,function(err)
+  {
+      if(!err)
+      {
+          console.log("Successfully deleted");
+          res.redirect("/");
+      }
+  })
+});
 
-const item2=new Item({
-    name:"Hello, Welcome to my word 1",
-})
-
-const item3=new Item({
-    name:"Hello, Welcome to my word 2",
-})
-
-const d=[item1,item2,item3];    
-Item.insertMany(d,function(err){
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log("Saved Successfully");
-    }
-})
-
-app.get("/", function(req,res){
-        //  res.send("It's me");
-        Item.find({},function(err,f){
-            res.render("list",{newListItems:i1});
-            // console.log(f);
-        })
-       
-}) 
-
-app.post("/", function(req,res){
-     i = req.body.m;
-    // console.log(i);
-    // res.render("list",{newListItem:i});
-    i1.push(i);
-    res.redirect("/");
-})
-
-app.listen(3000, ()=>{
-    console.log("It's working");
+app.listen(3000,function()
+{
+    console.log("Server is listening to port 3000");
 })
